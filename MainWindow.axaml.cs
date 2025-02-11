516d2f0120319eab27f0a415d92636a7297497a3
ABViney <vineyadrian@gmail.com>
2025-02-10 22:14:37 -0500

    Init commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FixedLengthFile_Cleaner;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
