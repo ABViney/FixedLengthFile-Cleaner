3f43ffb64c5d8bb894be5dba9da4f5d882251d49
ABViney <vineyadrian@gmail.com>
2025-02-24 05:59:39 -0500

    Add model for files being cleaned

---
+++ Models/CleanableFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace FixedLengthFile_Cleaner.Models;
+
+public enum CleanableFileType
+{
+    TextFile,
+    ZipFile,
+}
+
+public class CleanableFile
+{
+    public required string InputFilePath { get; set; }
+    public required string OutputFilePath { get; set; }
+    public required CleanableFileType FileType { get; set; }
+    public int NumberOfQuotes { get; set; }
+}
