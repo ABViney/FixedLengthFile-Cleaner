6b0543adf66b840f1ab8e5c0fe78cd285f141777
ABViney <vineyadrian@gmail.com>
2025-03-01 10:14:53 -0500

    Add StatusBar control

---
+++ Controls/StatusBar.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace FixedLengthFile_Cleaner.Controls;
+
+public partial class StatusBar : UserControl
+{
+    public StatusBar()
+    {
+        InitializeComponent();
+    }
+}
