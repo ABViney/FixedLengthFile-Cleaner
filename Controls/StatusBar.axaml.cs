cef6808b44535f8c8350d8154b7e56320afc01d3
ABViney <vineyadrian@gmail.com>
2025-03-13 04:27:52 -0400

    Add methods for updating status

--- Controls/StatusBar.axaml.cs
+++ Controls/StatusBar.axaml.cs
@@ -1,13 +1,25 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+﻿using Avalonia.Controls;
 
 namespace FixedLengthFile_Cleaner.Controls;
 
 public partial class StatusBar : UserControl
 {
+    private string _defaultCurrentActionText = "Awaiting input...";
+    
     public StatusBar()
     {
         InitializeComponent();
+        ResetStatus();
+    }
+
+    public void ResetStatus()
+    {
+        SetStatus(null, null);
+    }
+    
+    public void SetStatus(string? currentStatus, string? progressIndicator)
+    {
+        CurrentStatusTextBlock.Text = currentStatus ?? _defaultCurrentActionText;
+        CurrentProgressTextBlock.Text = progressIndicator ?? "";
     }
 }
