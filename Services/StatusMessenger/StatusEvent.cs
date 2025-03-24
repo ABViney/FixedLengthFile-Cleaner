89a4b3e24afc0237d562fffbe86183ac09b2b8be
ABViney <vineyadrian@gmail.com>
2025-03-24 14:04:57 -0400

    Add service for updating the status control outside the view

---
+++ Services/StatusMessenger/StatusEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FixedLengthFile_Cleaner.Services.StatusMessenger;
+
+public class StatusEvent : EventArgs
+{
+    public string? Message { get; }
+
+    public StatusEvent(string? message) => Message = message;
+}
---
+++ Services/StatusMessenger/StatusMessenger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FixedLengthFile_Cleaner.Services.StatusMessenger;
+
+public class StatusMessenger
+{
+    public event EventHandler<StatusEvent> StatusChanged;
+    public event EventHandler<ProgressEvent> ProgressChanged;
+    
+    private static StatusMessenger _instance;
+    
+    private StatusMessenger() { }
+
+    
+    public static StatusMessenger GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new StatusMessenger();
+        }
+        return _instance;
+    }
+
+    public void SetStatus(string? currentStatus)
+    {
+        var statusEvent = new StatusEvent(currentStatus);
+        StatusChanged?.Invoke(this, statusEvent);
+    }
+
+    public void SetProgress(string? progress)
+    {
+        var progressEvent = new ProgressEvent(progress);
+        ProgressChanged?.Invoke(this, progressEvent);
+    }
+}
